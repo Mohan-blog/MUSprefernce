79c3c3e8270d9551268babdbe936da2fe7715298
Mohan-blog <mohan.seelamneni@gmail.com>
2020-01-23 17:15:03 +0530

    multiple users with sharedprefernce

---
+++ app/src/main/java/com/adteccorp/muspexample/SuccssAct.java
@@ -0,0 +1,14 @@
+package com.adteccorp.muspexample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuccssAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_succss );
+    }
+}
